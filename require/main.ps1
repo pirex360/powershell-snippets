8df5f57a54b12a9b8941d4a9ff6c99b1e4737914
Filipe Pires <joaobessa@gmail.com>
2023-07-20 14:47:00 +0100

    Update main.ps1

--- require/main.ps1
+++ require/main.ps1
@@ -1,1 +1,14 @@
+# Simple Require / Include Concept in Powershell
 
+# :: GLOBAL VARIABLES
+$currentScriptPath = $PSCommandPath
+$scriptDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$includesPath = Join-Path -Path $scriptDirectory -ChildPath "includes\"
+
+# :: INCLUDES
+Invoke-Expression -Command ". '${includesPath}helpers.ps1'"
+
+# :: MAIN
+
+# function from the external file helpers.ps1
+helloFromTheOtherFile -name "Rambo"
